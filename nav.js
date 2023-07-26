7869c6eff2634949935a4d189599a22e07ece47c
Thomas McLaughlin <123tommygun1@gmail.com>
2023-07-25 23:10:49 -0400

    fixes

--- nav.js
+++ nav.js
@@ -1,10 +1,17 @@
-//
-//    Toggle Mobile Navigation
-//
+// Toggle Mobile Navigation
 const navbarMenu = document.querySelector("#navigation #navbar-menu");
 const hamburgerMenu = document.querySelector("#navigation .hamburger-menu");
+const navbarLinks = navbarMenu.querySelectorAll("a");
 
 hamburgerMenu.addEventListener('click', function() {
     navbarMenu.classList.toggle("open");
     hamburgerMenu.classList.toggle("clicked");
+});
+
+navbarLinks.forEach(function(navbarLink) {
+    navbarLink.addEventListener('click', function() {
+        // Close the navbarMenu and revert the hamburgerMenu state when a link is clicked
+        navbarMenu.classList.remove("open");
+        hamburgerMenu.classList.remove("clicked");
+    });
 });
